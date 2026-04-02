acca5b3d439ab1757c88e7c5be73c692f8151deb
Arpit Patidar <arpitpatidar851@gmail.com>
2026-04-02 16:28:55 +0530

    Increase body size limit for profile upload

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
